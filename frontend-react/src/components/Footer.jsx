d14b5e05ab4d9dc66699efa775d0b4526c5e92e1
stefanpaun700 <stefanpaun700@gmail.com>
2025-07-17 09:50:14 -0400

    home-page

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy; 2025 Stef Paun</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='p5 text-center bg-light-dark rounded'>
+          <h1 className='text-light'>Stock Prediction Portal</h1>
+          <p className='text-light lead'>
+            References to particular companies or securities, if any, are included for informational or illustrative purposes only and should not be considered as an endorsement by Capital Group. Views expressed regarding a particular company, security, industry or market sector should not be considered an indication of trading intent of any investment funds or current holdings of any investment funds. These views should not be considered as investment advice nor should they be considered a recommendation to buy or sell.
+          </p>
+          <Button text='Login' class='btn-outline-info' />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main
