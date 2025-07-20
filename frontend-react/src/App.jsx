4aa505f051f0b11ef3f4a385469f1f33856f6feb
stefanpaun700 <stefanpaun700@gmail.com>
2025-07-20 16:52:49 -0400

    Registration page details

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,14 +3,24 @@ import './assets/css/style.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Register from './components/Register'
+import Login from './components/Login'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 function App() {
   
   return (
     <>
+    <BrowserRouter >
       <Header />
-      <Main />
+      <Routes>
+        <Route  path='/' element={<Main />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
       <Footer />
+    </BrowserRouter>
     </>
   )
 }
